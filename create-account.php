39d8f95a1f8c8f697168ea4e6273da6edb371d2e
Ciborg085 <1903bernardo@gmail.com>
2019-01-31 22:15:21 +0000

    Update create-account.php

--- create-account.php
+++ create-account.php
@@ -7,12 +7,19 @@
 		$password = $_POST['password'];
 		$email = $_POST['email'];
 
+		$n_class = $_POST['n_class'];
+		$l_class = $_POST['l_class'];
+		$class = $n_class . $l_class;
+		echo $class;
+		$class_n = $_POST['class_n'];
+		$student_n = $_POST['student_n'];
+
 		if (DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$username)))
 		{
 			#Checks if user already exists
 			echo 'User already exists';
 
-		} elseif ($username == null || $password == null || $email == null) 
+		}  elseif ($username == null || $password == null || $email == null) 
 		{
 			#Checks if all fields are filled
 			echo 'Please fill all fields required';
@@ -49,7 +56,7 @@
 		} else 
 		{
 			#Creates account on the database
-			DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email)', array(':username'=>$username, ':password'=>password_hash($password, PASSWORD_BCRYPT), ':email'=>$email));
+			DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email, :class, :class_n, :student_n)', array(':username'=>$username, ':password'=>password_hash($password, PASSWORD_BCRYPT), ':email'=>$email, ':class'=>$class, ':class_n'=>$class_n, ':student_n'=>$student_n));
 			echo 'Account Created';
 		}
 	}
@@ -58,13 +65,45 @@
 <html>
 <head>
 	<title>Create Account</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/styles_global.css">
+	<link rel="stylesheet" type="text/css" href="css/create-account.css">
+
 </head>
 <body>
-	<form method="post" action="create-account.php">
+	<form method="post" action="create-account.php" class="create-account">
 		<h2>Registar</h2>
-		<input type="text" name="username" value="" placeholder="Username"><p />
-		<input type="password" name="password" value="" placeholder="Password"><p />
+	
+		<input type="text" name="username" value="" placeholder="Nome de utilizador"><p />
+		<input type="password" name="password" value="" placeholder="Palavra Pass"><p />
 		<input type="text" name="email" value="" placeholder="Email"><p />
+		<input type="text" name="student_n" value="" placeholder="Número de estudante"><p />
+
+		<div class="school_class">
+			<div class="choices_container">
+				<select id="n_year" name="n_class">
+					<option value="">Ano</option>
+					<option value="8">8º Ano</option>
+					<option value="9">9º Ano</option>
+					<option value="10">10º Ano</option>
+					<option value="11">11º Ano</option>
+					<option value="12">12º Ano</option>
+				</select>
+				<select id="l_year" name="l_class">
+					<option>Turma</option>
+					<option value="A">A</option>
+					<option value="B">B</option>
+					<option value="C">C</option>
+					<option value="D">D</option>
+					<option value="E">E</option>
+					<option value="F">F</option>
+					<option value="H">H</option>
+					<option value="I">I</option>
+					<option value="J">J</option>
+				</select>
+			</div>
+		</div>
+		<input type="text" name="class_n" value="" placeholder="Número de turma">
 		<input type="submit" name="create-account" value="Registar">
 	</form>
 </body>
